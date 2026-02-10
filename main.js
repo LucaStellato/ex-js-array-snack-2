370fe1fb859aa88aaed26051a73dd31f9cbc8321
LucaStellato <lucastellato435@gmail.com>
2026-02-10 11:17:53 +0100

    snack 3 complete

--- main.js
+++ main.js
@@ -47,6 +47,8 @@ const books = [
 
 
 //SNACK 1
+
+//Stampa in console ogni titolo nella console
 //Crea un array (longBooks) con i libri che hanno più di 300 pagine;
 
 const longBooks = books.filter((book) => book.pages > 300)
@@ -60,3 +62,45 @@ const longBooksTitles = longBooks.map((book) => {
 })
 
 console.log(longBooksTitles)
+
+
+
+//SNACK 2
+//Creare un array (availableBooks) che contiene tutti i libri disponibili.
+
+const availableBooks = books.filter((book) => book.available === true)
+console.log(availableBooks)
+
+/*Crea un array(discountedBooks) con gli availableBooks, 
+ ciascuno con il prezzo scontato del 20 % 
+ (mantieni lo stesso formato e arrotonda al centesimo)*/
+
+
+
+
+//SNACK 3
+//Creare un array (authors) che contiene gli autori dei libri.
+const authors = books.map((book) => {
+    return book.author
+})
+
+
+//Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+
+const areAuthorsAdults = authors.every((author) => author.age >= 18)
+
+console.log(areAuthorsAdults)
+
+/*Ordina l’array authors in base all’età, senza creare un nuovo array.
+(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)*/
+
+if (areAuthorsAdults === true) {
+    authors.sort((a, b) => {
+        return a.age - b.age;
+    })
+} else {
+    authors.sort((a, b) => {
+        return b.age - a.age;
+    })
+}
+console.log(authors)
